f3752c302006dbaae3385ec448a225fb3b9dd6d4
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-27 18:52:13 +0800

    rearranged directory and placed public and src folders inside clients folder. added middleware directory to store authentication middleware (check if user is logged in. Started laying out front end react setup with navbar, home, login, profile, footer.

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/nav/Nav";
+import Home from "./components/homepage/Home";
+import Login from "./components/login/Login"
+import Profile from "./components/footer/Profile";
+import Footer from "./components/footer/Footer";
+
+import "./App.css";
+
+function App() {
+	return (
+		<div className="afflatus-music-app">
+			<Nav />
+			<main>
+				<Router>
+					<Switch>
+						<Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+						<Route exact path="/profile" component={Profile} />
+					</Switch>
+				</Router>
+			</main>
+			<Footer />
+		</div>
+	);
+}
+
+export default App;

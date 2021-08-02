5b0e6bf9e958186cdfaf5d5d7c424592d1cfb85e
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-02 16:33:51 +0800

    created initial version of Footer component

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer class="footer">
+            <div>
+                <div class="credits">
+                    <p>Aaron Au Yoong Copyright (c) 2021-Present. All rights reserved. </p>
+                </div>
+                <div class="icon-links">
+                    <a href="https://www.github.com/aaronauyoong"><i class="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/aaron-au-yoong-a69213142/"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="mailto:auyoong.aaron@gmail.com"><i class="fas fa-envelope-square"></i></a>
+                </div>
+            </div>         
+        </footer>
+    )
+}
+
+export default Footer;

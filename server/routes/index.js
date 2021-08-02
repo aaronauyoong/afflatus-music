7c25356c4099a454a350d5f22d137c796e2f37d9
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-02 15:32:38 +0800

    deleted extra use-auth.js file. Added routes from homeRoutes.js into index.js in routes backend folder.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const homeRoutes = require("./homeRoutes");
 
+// Link to defined routes
 router.use("/", homeRoutes);
 
 module.exports = router;

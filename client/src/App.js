68a43c33bc4dcb2256b0a663bede4ede1ab6c738
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-02 19:08:47 +0800

    moved footer to only display during login page (might rename it to something else later), as that is giving credit to myself for creating this website. Upon loading of main dashboard, it should only be the music player displaying at the bottom of the page

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Nav from "./components/nav/Nav";
 // import Home from "./components/homepage/Home";
 // import Login from "./components/login/Login";
 // import Profile from "./components/footer/Profile";
-import Footer from "./components/footer/Footer";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -31,7 +30,6 @@ function App() {
 					</Switch>
 				</Router>
 			</main>
-			<Footer />
 		</div>
 	);
 }
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Footer from "../footer/Footer";
 import "../../assets/styles/customStyles.css";
 import MusicCartoon from "../../assets/images/undrawMusicCartoon.svg";
 
@@ -7,29 +8,29 @@ const CLIENT_ID = "2ae77a009ef04f15b6de9046ff925ebb";
 const REDIRECT_URI = "http://localhost:3000";
 // Note: may need to add more scopes for playlist functionality.
 // For now, only adding basic functionality.
-const scopes = "streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20playlist-modify-public";
+const scopes =
+	"streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20playlist-modify-public";
 
-const AUTH_URL =
-	`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${scopes}`;
-
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${scopes}`;
 
 export default function Login() {
 	return (
-		<div className="login-page">
-			<div className="login-image">
-				<img
-					src={MusicCartoon}
-					alt="This is a cartoon of a bird listening to music."
-				/>
+		<div>
+			<div className="login-page">
+				<div className="login-image">
+					<img
+						src={MusicCartoon}
+						alt="This is a cartoon of a bird listening to music."
+					/>
+				</div>
+				<Container className="login-button">
+					<a className="btn btn-success btn-lg" href={AUTH_URL}>
+						{" "}
+						Login with Spotify{" "}
+					</a>
+				</Container>
 			</div>
-			<Container
-				className="login-button"
-			>
-				<a className="btn btn-success btn-lg" href={AUTH_URL}>
-					{" "}
-					Login with Spotify{" "}
-				</a>
-			</Container>
+			<Footer />
 		</div>
 	);
 }

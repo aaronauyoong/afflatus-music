dad0e7c387c4ebd7d1be2faac0d36316cf58f024
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-02 19:34:46 +0800

    added mongoose to list of packages in back end. wrote code for initial setup/connection to mongodb. added to .env.example MONGODB_URI.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
 const routes = require("./routes");
-require("dotenv").config();
+
 
 const app = express();
+require("dotenv").config();
+
 // To prevent CORS errors
 app.use(cors());
 
@@ -14,9 +18,14 @@ app.use(routes);
 const PORT = process.env.PORT || 3001;
 
 // Connection to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/afflatusmusic", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+});
 
 // Start the API server
 app.listen(PORT, () =>
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
-

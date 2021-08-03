82b0092741d6f545a52a327a973e95a54697f7bf
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-03 16:44:58 +0800

    moved mongoose connection to config/connection.js file. Added initial version of schemas/index.js, schemas/resolvers.js and schemas/typeDefs.js to aid in setting up Apollo server and GraphQL

--- server/server.js
+++ server/server.js
@@ -1,31 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
 const routes = require("./routes");
-
-
-const app = express();
 require("dotenv").config();
+
+// Import the ApolloServer class
+const { ApolloServer } = require("apollo-server-express");
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 // To prevent CORS errors
 app.use(cors());
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+// Update Express.js to use Apollo server features
+server.applyMiddleware({ app });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-const PORT = process.env.PORT || 3001;
-
-// Connection to Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/afflatusmusic", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-});
-
 // Start the API server
-app.listen(PORT, () =>
-	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-);
+// app.listen(PORT, () =>
+// 	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+// );
+
+db.once("open", () => {
+	app.listen(PORT, () => {
+		console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+		console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+	});
+});

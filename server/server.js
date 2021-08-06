3f301fafb545758aa19852f037cee7aa329e34df
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-07 01:24:52 +0800

    updated packages used to include apollo-server-express, path, imported typeDefs, resolvers etc. removed routes requirement. added code for production.

--- server/server.js
+++ server/server.js
@@ -1,38 +1,36 @@
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 const cors = require("cors");
-const routes = require("./routes");
-require("dotenv").config();
-
-// Import the ApolloServer class
-// const { ApolloServer } = require("apollo-server-express");
-
-// Import the two parts of a GraphQL schema
-// const { typeDefs, resolvers } = require("./schemas");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: authMiddleware,
+});
 
-// To prevent CORS errors
 app.use(cors());
-
-// Create a new instance of an Apollo server with the GraphQL schema
-// const server = new ApolloServer({
-// 	typeDefs,
-// 	resolvers,
-// });
-
-// Update Express.js to use Apollo server features
-// server.applyMiddleware({ app });
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use(express.json());
 
-// Start the API server
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 db.once("open", () => {
 	app.listen(PORT, () => {
 		console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-		// console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+		console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 	});
 });

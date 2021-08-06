c3732316570b7f4dea3a57430f9bb121c2a670fb
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-07 01:21:17 +0800

    removed routes folder as will be relying fully on Apollo and GraphQL for this application

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeRoutes = require("./homeRoutes");
-
-// Link to defined routes
-router.use("/", homeRoutes);
-
-module.exports = router;

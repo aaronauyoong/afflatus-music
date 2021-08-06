fd4fbb83268e0c748536e85b7f21a76664ccc03b
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-06 16:03:56 +0800

    refactored so post seeds are in its own postSeeds.json file. Cleaned seeds.js file

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,41 +1,38 @@
 const db = require("../config/connection");
 const { User, Post, Comment } = require("../models");
-const userSeeds = require("./userSeeds.json")
+const userSeeds = require("./userSeeds.json");
+const postSeeds = require("./postSeeds.json");
 
 db.once("open", async () => {
-	await Comment.deleteMany();
+	try {
+		await Comment.deleteMany();
+		// Comment seeds to be inserted.
 
-	console.log("//----------------------------Comments seeded----------------------------//");
+		console.log(
+			"//----------------------------Comments seeded----------------------------//"
+		);
 
-	await Post.deleteMany();
+		await Post.deleteMany();
+		await Post.insertMany(postSeeds);
 
-	const posts = await Post.insertMany([
-		{
-			postTitle: "Check out my playlist!",
-			postContent:
-				"Omg I love this album! https://open.spotify.com/album/4igVZxLMxQDV7D94RzW6cw?si=4dHBrISkQlSyQAyR4PefhA.",
-			createdAt: "06/06/2021",
-			username: "ronnieyoong"
-		},
-		{
-			postTitle: "Check out this album!",
-			postContent:
-				"Hey guys, check out this playlist! What songs do you think should be on here? https://open.spotify.com/playlist/5OmCKHlEBTzjATHRHJiXJU?si=RlZ6WGy9QaaavJetuR8XmQ.",
-			createdAt: "06/06/2021",
-			username: "daniel.k.here"
-		}
-	]);
+		console.log(
+			"//----------------------------Posts seeded----------------------------//"
+		);
 
-	console.log("//----------------------------Posts seeded----------------------------//");
+		await User.deleteMany();
+		await User.create(userSeeds);
 
-	await User.deleteMany();
+		console.log(
+			"//----------------------------Users seeded----------------------------//"
+		);
 
-	await User.create(userSeeds);
-
-
-	console.log("//----------------------------Users seeded----------------------------//");
-
-	
-	console.log("//----------------------------ALL DONE!----------------------------//!");
-	process.exit();
+		console.log(
+			"//----------------------------ALL DONE!----------------------------//!"
+		);
+		process.exit();
+	} catch (err) {
+		console.error(err);
+		console.log("Please check the error message.");
+		process.exit(1);
+	}
 });

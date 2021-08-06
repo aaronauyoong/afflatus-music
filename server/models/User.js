92715125be11e4a2bba8067ba1489dd33dd6167d
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-06 14:45:05 +0800

    installed bcrypt npm package. updated User Model

--- server/models/User.js
+++ server/models/User.js
@@ -2,25 +2,43 @@
 // Updated model as per Spotify's API
 
 const { Schema, model } = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-	id: {
-		type: Number,
-		required: true,
-	},
 	username: {
 		type: String,
 		required: true,
 	},
-    email: {
+	email: {
 		type: String,
 		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 5,
 	},
 	displayName: {
 		type: String,
 		required: true,
-	}
+	},
 });
+
+// set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+	if (this.isNew || this.isModified("password")) {
+		const saltRounds = 10;
+		this.password = await bcrypt.hash(this.password, saltRounds);
+	}
+
+	next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
 
 const User = model("User", userSchema);
 

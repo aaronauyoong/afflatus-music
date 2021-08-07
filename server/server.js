27fa99a68a88cb105d993bb0e7593c3646727539
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-07 14:25:27 +0800

    added server.applyMiddleware in server.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const server = new ApolloServer({
 	context: authMiddleware,
 });
 
+server.applyMiddleware({ app });
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("*", (req, res) => {
+app.get("*", (_, res) => {
 	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 

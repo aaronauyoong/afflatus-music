0c79c5e2b75668c20e48544fc03c1a220364b7b4
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-07 23:19:05 +0800

    updated seed files

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,16 +1,16 @@
 const db = require("../config/connection");
-const { User, Post, Comment } = require("../models");
+const { User, Post } = require("../models");
 const userSeeds = require("./userSeeds.json");
 const postSeeds = require("./postSeeds.json");
 
 db.once("open", async () => {
 	try {
-		await Comment.deleteMany({});
-		// Comment seeds to be inserted.
+		// await Comment.deleteMany({});
+		// // Comment seeds to be inserted.
 
-		console.log(
-			"//----------------------------Comments seeded----------------------------//"
-		);
+		// console.log(
+		// 	"//----------------------------Comments seeded----------------------------//"
+		// );
 
 		await Post.deleteMany({});
 		console.log("Old test post data deleted.")

7cd08b23b9c30f4c544ca9c5cfb436f4e374b2b4
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-08 01:44:45 +0800

    updated styling for spotifylogin page

--- client/src/components/login/SpotifyLogin.js
+++ client/src/components/login/SpotifyLogin.js
@@ -13,17 +13,16 @@ const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&
 
 export default function SpotifyLogin() {
 
-
 	return (
 		<div>
-			<div className="login-page">
-				<div className="login-image">
+			<div className="spotify-login-page">
+				<div className="spotify-login-image">
 					<img
 						src={MusicCartoon}
 						alt="This is a cartoon of a bird listening to music."
 					/>
 				</div>
-				<Container className="login-button">
+				<Container className="spotify-login-button">
 					<a className="btn btn-success btn-lg" href={AUTH_URL}>
 						Login with Spotify
 					</a>

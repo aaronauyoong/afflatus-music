75890953f4f0cbf38b340068d393602fc0966f83
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-08 21:47:03 +0800

    changed naming from posts to thoughts. Added a 'NewThought.js' file where users can fill in a form to submit their thoughts and recommendations of current music, albums, playlists etc. Added a ThoughtCard file, where each thought in the database will be rendered using that template.

---
+++ client/src/components/thoughts/ThoughtCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ThoughtList = ({ thoughts, title }) => {
+	if (!thoughts.length) {
+		return <h3>No music thoughts nor playlists have been shared yet. Be the first to share your thoughts!</h3>;
+	}
+
+	return (
+		<div>
+			<h3>{title}</h3>
+			{thoughts &&
+				thoughts.map((thought) => (
+					<div key={thought._id} className="card mb-3">
+						<h4 className="card-header bg-primary text-light p-2 m-0">
+							{thought.thoughtAuthor} <br />
+							<span style={{ fontSize: "1rem" }}>
+								had this thought on {thought.createdAt}
+							</span>
+						</h4>
+						<div className="card-body bg-light p-2">
+							<p>{thought.thoughtContent}</p>
+						</div>
+						<Link
+							className="btn btn-primary btn-block btn-squared"
+							to={`/thoughts/${thought._id}`}
+						>
+							Ponder more about this Thought.
+						</Link>
+					</div>
+				))}
+		</div>
+	);
+};
+
+export default ThoughtList;

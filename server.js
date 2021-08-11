59d9883d8328702d4084583e5ea14a13df306a3c
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-08-11 16:40:45 +0800

    hopefully it should be all good now pls

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
-const spotifyRoutes = require("./routes/spotifyRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,7 +18,6 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(spotifyRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "client", "build")));
